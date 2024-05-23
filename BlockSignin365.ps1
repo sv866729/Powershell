66ca17fc316f10442fd669ace951758a6c45713d
sv866729 <96221462+sv866729@users.noreply.github.com>
2024-05-23 11:07:43 -0500

    Add files via upload
    This is a building block for a bigger script but is used to Block all sign ins on Microsoft 365

---
+++ BlockSignin365.ps1
@@ -0,0 +1,4 @@
+﻿# This will block all sign ins for all accounts execpt that running the script
+connect-msolservice
+$users = get-msoluser
+forEach($user in $users){Set-MsolUser -UserPrincipalName $user.UserPrincipalName -BlockCredential $true}
