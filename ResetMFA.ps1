02397677a57416fc6ebcdff1dd93b95bb056d9fd
sv866729 <96221462+sv866729@users.noreply.github.com>
2024-05-23 11:17:35 -0500

    Add files via upload
    This is used to reset MFA for all users in a 365 tenant

---
+++ ResetMFA.ps1
@@ -0,0 +1,6 @@
+﻿# Used to reset every users MFA options in m365
+Connect-MsolService
+$users = Get-MsolUser
+foreach ($user in $users){
+    Reset-MsolStrongAuthenticationMethodByUpn -UserPrincipalName $user.UserPrincipalName
+}
