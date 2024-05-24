668c5aeecdac0c1ac5aa69a62d5f982f1e6710da
SV Powershell <96221462+sv866729@users.noreply.github.com>
2024-05-24 14:29:04 -0500

    Add files via upload

---
+++ PassphraseFuntion.ps1
@@ -0,0 +1,14 @@
+﻿# Passphase generator
+function GeneratePassphrase ([array]$wordlist, [int]$wordcount) {
+    $passphrase = ""
+    for ($i = 1; $i -le $wordcount; $i++){
+        [int]$randomnumber = get-random -Minimum 0 -Maximum $wordlist.Length
+        [string]$randomword = $wordlist[$randomnumber]
+        [string]$randomnumber = get-random -Minimum 0 -Maximum 9
+        $passphrase += [string]$randomword + $randomnumber + "-"
+        
+    }
+    [string]$randomnumber = get-random -Minimum 0 -Maximum 1000
+    $passphrase += $randomnumber
+    return $passphrase.Trim()
+}
