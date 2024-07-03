d2b4ab396029c893ca9e8dfb48d26b1f2a02078f
SV Powershell <96221462+sv866729@users.noreply.github.com>
2024-07-02 21:54:26 -0500

    Update defaultfuntions.ps1

--- defaultfuntions.ps1
+++ defaultfuntions.ps1
@@ -1,1 +1,19 @@
-
+#Basic Error handling funtion to reduce code amount
+function error-handling {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$command,
+        [string]$Successful_message = "Successfully executed: $command",
+        [string]$error_message = "Error executing: $command",
+        [string]$error_command = 'return'
+    )
+    
+    try {
+        Invoke-Expression $command
+        Write-Host $Successful_message -ForegroundColor Green
+    }
+    catch {
+        Write-Host $error_message -ForegroundColor Red
+        Invoke-Expression $error_command
+    }
+}
