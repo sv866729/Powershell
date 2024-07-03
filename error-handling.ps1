35d36d077319fb58b3805457ae56f3bec79ba878
SV Powershell <96221462+sv866729@users.noreply.github.com>
2024-07-02 21:53:15 -0500

    Create error-handling.ps1

---
+++ error-handling.ps1
@@ -0,0 +1,18 @@
+function error-handling {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$command,
+        [string]$Successful_message = "Successfully executed: $command",
+        [string]$error_message = "Error executing: $command",
+        [string]$error_command = 'return'
+    )
+    
+    try {
+        Invoke-Expression $command
+        Write-Host $Successful_message -ForegroundColor Green
+    }
+    catch {
+        Write-Host $error_message -ForegroundColor Red
+        Invoke-Expression $error_command
+    }
+}
