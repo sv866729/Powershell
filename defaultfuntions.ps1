8997a7a06b613975489229ad7ac9a63ac1ac29b1
SV Powershell <96221462+sv866729@users.noreply.github.com>
2024-07-02 21:56:10 -0500

    Update defaultfuntions.ps1

--- defaultfuntions.ps1
+++ defaultfuntions.ps1
@@ -1,4 +1,7 @@
-#Basic Error handling funtion to reduce code amount
+#############################################
+# Basic error handling for running commands #
+#############################################
+
 function error-handling {
     param(
         [Parameter(Mandatory=$true)]
@@ -15,5 +18,25 @@ function error-handling {
     catch {
         Write-Host $error_message -ForegroundColor Red
         Invoke-Expression $error_command
+    }
+}
+
+################################
+# Install the module if needed #
+################################
+
+function Install-ModuleIfNeeded {
+    param (
+        [string]$ModuleName
+    )
+    if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
+        try {
+            Install-Module -Name $ModuleName -Scope CurrentUser -Force
+            Write-Host "Module $ModuleName installed successfully." -ForegroundColor Green
+        } catch {
+            Write-Host "Failed to install module $ModuleName. Error: $_" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "Module $ModuleName is already installed." -ForegroundColor Yellow
     }
 }
